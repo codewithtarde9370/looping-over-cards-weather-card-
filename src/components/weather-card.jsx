63b18f7eb218bcbe749796c19f7f41b5aa624b9e
codewithtarde9370 <teja36491@gmail.com>
2024-06-04 18:39:12 +0530

    added changes

--- src/components/weather-card.jsx
+++ src/components/weather-card.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import "./weather-card.css";
+import Cloudy from './cloudy.png'
+import Sunny from './sunny.png'
+
+import Rainy from './rainy-day.png'
+
+import Snowy from './snowy.png'
 
 
-function WeatherCard({ imgSrc, city, temp, weather }) {
+
+function WeatherCard({city, temp, weather }) {
+    const WEATHERICONS=
+    {
+        "Sunny":Sunny,
+        "Cloudy":Cloudy,
+        "Rainy":Rainy,
+        "Snowy":Snowy
+    }
+    
+
 
     
   return (
     <div className="card">
-      <img src={imgSrc} alt="Weather" className="img" />
+      <img src={WEATHERICONS[weather]} alt="Weather" className="img" />
       <h1 className="city">{city}</h1>
       <p className="temp">{temp}</p>
       <p className="weather">{weather}</p>
     </div>
   );
-}
 
+}
 export default WeatherCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WeatherCard from './components/weather-card';
-import Sunny from './components/sunny.png';
 import "./main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,30 +8,28 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <h1>Weather Cards</h1>
     <div className='card-container'>
       
-      <WeatherCard 
-        imgSrc={Sunny} 
-        city="New York" 
-        temp="75°F" 
-        weather="Sunny" 
-      />
-      <WeatherCard 
-        imgSrc={Sunny} 
-        city="New York" 
-        temp="75°F" 
-        weather="Sunny" 
-      />
-      <WeatherCard 
-        imgSrc={Sunny} 
-        city="New York" 
-        temp="75°F" 
-        weather="Sunny" 
-      />
-      <WeatherCard 
-        imgSrc={Sunny} 
-        city="New York" 
-        temp="75°F" 
-        weather="Sunny" 
-      />
+    <WeatherCard 
+  city="Mumbai" 
+  temp="29°C" 
+  weather="Sunny" 
+/>
+<WeatherCard 
+  city="Pune" 
+  temp="22°C" 
+  weather="Rainy" 
+/>
+<WeatherCard 
+  city="Nagpur" 
+  temp="25°C" 
+  weather="Cloudy" 
+/>
+<WeatherCard 
+  city="Nashik" 
+  temp="15°C" 
+  weather="Snowy" 
+/>
+
+
       
     </div>
   </>
